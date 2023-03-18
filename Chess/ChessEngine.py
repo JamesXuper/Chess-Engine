b02ece0f872023c9f653500052c7878e1b62e3fa
James Xu <122507551+JamesXuper@users.noreply.github.com>
2023-03-18 20:33:10 +1100

    added Rook moves

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -85,7 +85,7 @@ class GameState():
                 if self.board[r-1][c-1][0] == 'b':
                     moves.append(Move((r,c), (r-1,c-1), self.board))
                     
-            if c+1 < 7: #captures to the right
+            if c+1 <= 7: #captures to the right
                 if self.board[r-1][c+1][0] == 'b':
                     moves.append(Move((r,c), (r-1,c+1), self.board))
     
@@ -99,13 +99,29 @@ class GameState():
                 if self.board[r+1][c-1][0] == 'w':
                     moves.append(Move((r,c), (r+1,c-1), self.board))
                     
-            if c+1 < 7: #captures to the right
+            if c+1 <= 7: #captures to the right
                 if self.board[r+1][c+1][0] == 'w':
                     moves.append(Move((r,c), (r+1,c+1), self.board))
             
     
     def getRookMoves(self,r,c,moves):
-        pass
+        directions = ((-1,0),(0,-1),(1,0),(0,1))
+        enemyColor = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1,8):
+                endRow = r+d[0]*i
+                endCol = c+d[1]*i
+                if 0 <= endRow < 8 and 0 <= endCol < 8:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":
+                        moves.append(Move((r,c), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColor:
+                        moves.append(Move((r,c), (endRow, endCol), self.board))
+                        break
+                    else:
+                        break
+                else:
+                    break
 
     def getKnightMoves(self,r,c,moves):
         pass
